0ede177ef866e8a1c6c64ee1d3cd8595049784a9
Saleh Khazaei <saleh.khazaei@gmail.com>
2024-07-26 19:47:07 +0400

    fix: Fixes setup script

--- scripts/setup_flux.sh
+++ scripts/setup_flux.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-export GITHUB_TOKEN="<value>"
-
 set -e
+set -x
+
+read -p "Path of ssh key to use for git (read and write access required): " -r
+echo
+
+KEY_FILE="$REPLY"
+
+SSH_KEY_ALGORITHM="ecdsa"
+read -p "SSH Key algorithm (rsa, ecdsa or ed25519, default: ecdsa): " -r
+echo
+
+if [[ ! -z "$REPLY" ]]; then
+  SSH_KEY_ALGORITHM="$REPLY"
+fi
 
 if [[ -f scripts/vars.sh ]]; then
   source scripts/vars.sh
@@ -13,10 +25,11 @@ if [[ -z $FLUX ]]; then
   curl -s https://fluxcd.io/install.sh | sudo bash
 fi
 
-flux bootstrap github \
+flux bootstrap git \
   --components-extra=image-reflector-controller,image-automation-controller \
-  --owner=kaytu-io \
-  --repository=kaytu-flux \
+  --url=ssh://git@github.com/kaytu-io/kaytu-flux.git \
   --branch=main \
+  --recurse-submodules \
   --path=clusters/dev \
-  --token-auth
+  --private-key-file="$KEY_FILE" \
+  --ssh-key-algorithm=$SSH_KEY_ALGORITHM
